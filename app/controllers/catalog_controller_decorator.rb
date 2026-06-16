8d35a2e4d10f74a00557d22689145c1f59870f54
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2026-06-16 13:48:04 -0400

    Update pagination settings in CatalogController
    Adjust pagination settings for advanced search in CatalogController.

--- app/controllers/catalog_controller_decorator.rb
+++ app/controllers/catalog_controller_decorator.rb
@@ -5,5 +5,9 @@ module CatalogControllerDecorator
   # This code runs when the decorator is loaded (in to_prepare)
   CatalogController.configure_blacklight do |config|
     config.advanced_search[:form_facet_partial] = "advanced_search_facets"
+
+    # adjust pagination
+    config.per_page = [6, 12, 24, 48, 96]
+    config.default_per_page = 12    
   end
 end

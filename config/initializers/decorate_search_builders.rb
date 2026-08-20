dc25b9878e243b6bca35905223b8cf04cf982f11
Tracy A McCormick <tracy.alan.mccormick@gmail.com>
2026-08-20 15:50:56 -0400

    debug: add method inspection logging to verify decorator application

--- config/initializers/decorate_search_builders.rb
+++ config/initializers/decorate_search_builders.rb
@@ -27,23 +27,31 @@ Rails.application.config.to_prepare do
   # Decorate search builders to enforce facet limits
   Rails.logger.info "=== FacetLimitEnforcer: Starting decorator setup ==="
   
+  # Check AdvSearchBuilder
   begin
+    Rails.logger.info "=== AdvSearchBuilder methods before prepend: #{AdvSearchBuilder.instance_methods(false).first(5).inspect} ==="
+    
     if AdvSearchBuilder.include?(FacetLimitEnforcer)
       Rails.logger.info "=== FacetLimitEnforcer: Already applied to AdvSearchBuilder ==="
     else
       AdvSearchBuilder.prepend(FacetLimitEnforcer)
       Rails.logger.info "=== FacetLimitEnforcer: Successfully prepended to AdvSearchBuilder ==="
+      Rails.logger.info "=== AdvSearchBuilder methods after prepend: #{AdvSearchBuilder.instance_methods(false).first(5).inspect} ==="
     end
   rescue NameError => e
     Rails.logger.warn("=== FacetLimitEnforcer: AdvSearchBuilder not found: #{e.message} ===")
   end
   
+  # Check IiifPrint::CatalogSearchBuilder
   begin
+    Rails.logger.info "=== IiifPrint::CatalogSearchBuilder methods before prepend: #{IiifPrint::CatalogSearchBuilder.instance_methods(false).first(5).inspect} ==="
+    
     if IiifPrint::CatalogSearchBuilder.include?(FacetLimitEnforcer)
       Rails.logger.info "=== FacetLimitEnforcer: Already applied to IiifPrint::CatalogSearchBuilder ==="
     else
       IiifPrint::CatalogSearchBuilder.prepend(FacetLimitEnforcer)
       Rails.logger.info "=== FacetLimitEnforcer: Successfully prepended to IiifPrint::CatalogSearchBuilder ==="
+      Rails.logger.info "=== IiifPrint::CatalogSearchBuilder methods after prepend: #{IiifPrint::CatalogSearchBuilder.instance_methods(false).first(5).inspect} ==="
     end
   rescue NameError, NoMethodError => e
     Rails.logger.warn("=== FacetLimitEnforcer: IiifPrint::CatalogSearchBuilder not found: #{e.message} ===")
